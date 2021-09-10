aa1683ce4efee4bee5779e4de639204d12e006ad
Taylor Stearns <t@tstearns.com>
2021-09-10 15:19:45 -0400

    switch away from benchmark, update for 2019

--- txt-to-json.sh
+++ txt-to-json.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Run this in the same directory as the benchmark factor annual files downloaded from Ken French:
+# Run this in the same directory as the annual factor files downloaded from Ken French:
 # http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html
 
 echo "[" > ff-mkt.json
-awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $2/100)}' < F-F_Benchmark_Factors_Annual.txt >> ff-mkt.json
+awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $2/100)}' < F-F_Research_Data_Factors.txt >> ff-mkt.json
 echo "]" >> ff-mkt.json
 
 echo "[" > ff-smb.json
-awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $3/100)}' < F-F_Benchmark_Factors_Annual.txt >> ff-smb.json
+awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $3/100)}' < F-F_Research_Data_Factors.txt >> ff-smb.json
 echo "]" >> ff-smb.json
 
 echo "[" > ff-hml.json
-awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $4/100)}' < F-F_Benchmark_Factors_Annual.txt >> ff-hml.json
+awk 'NR>1 {printf("\t\{ \"dt\": %s, \"r\": %s \},\n", $1, $4/100)}' < F-F_Research_Data_Factors.txt >> ff-hml.json
 echo "]" >> ff-hml.json
